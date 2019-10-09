a9ed5e826a60c041f30711c5c8d2c6c572c847f1
Unknown <miguela_nunez@outlook.com>
2019-10-09 13:02:27 -0500

    add Login and Boostrap
    Se agrego todo el Login y las carpetas de Boostrap

--- banco/banco/urls.py
+++ banco/banco/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Apps.analisis import views as viewsBanco
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login', viewsBanco.login, name='login' )
 ]
